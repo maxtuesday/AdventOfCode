1d4bb9e98ed30a28ecdc1b4d61d5aa092b1c7f9e
maxtuesday <jonesmtuesday@gmail.com>
2022-12-24 20:59:00 -0500

    2022 day 12 - rust - refactor

--- 2022/rust/day_12/src/lib.rs
+++ 2022/rust/day_12/src/lib.rs
@@ -13,96 +13,89 @@ struct HeightMap {
 }
 
 impl HeightMap {
-    fn get_height(&self, point: &Point) -> i32 {
-        self.map[point.r as usize][point.c as usize]
+    fn from(input: &str) -> Self {
+        let mut start = Point { r: 0, c: 0 };
+        let mut end = Point { r: 0, c: 0 };
+        let map: Vec<Vec<i32>> = input.lines().enumerate().map(|(r, line)| {
+            line.chars().enumerate().map(|(c, char)| {
+                match char {
+                    'S' => {
+                        start = Point { r: r as i32, c: c as i32 };
+                        0
+                    }
+                    'E' => {
+                        end = Point { r: r as i32, c: c as i32 };
+                        26
+                    }
+                    _ => char as i32 - 'a' as i32
+                }
+            }).collect()
+        }).collect();
+        Self { map, start, end }
     }
 
-    // BFS
-    fn find_min_path(&self, start: &Point) -> Option<i32> {
+    fn get_height(&self, point: &Point) -> Option<i32> {
+        self.map.get(point.r as usize)
+            .and_then(|row| row.get(point.c as usize))
+            .and_then(|&height| Some(height))
+    }
+
+    fn find_min_path_to_height(&self, start: &Point, end_height: i32, calc_height_diff: fn(from: i32, to: i32) -> i32) -> Option<i32> {
         let mut step = 0;
         let mut queue: VecDeque<Point> = VecDeque::from([start.clone()]);
         let mut visited: HashSet<Point> = HashSet::from([start.clone()]);
-        let height = self.map.len() as i32;
-        let width = self.map[0].len() as i32;
-        let dr = vec![1, 0, -1, 0];
-        let dc = vec![0, 1, 0, -1];
 
         while queue.len() > 0 {
-            let cur_len = queue.len();
-            for _ in 0..cur_len {
+            let cur_queue_len = queue.len();
+            for _ in 0..cur_queue_len {
                 let pos = queue.pop_front().unwrap();
-                if pos == self.end {
+                let pos_height = self.get_height(&pos).unwrap();
+                if pos_height == end_height {
                     return Some(step);
                 }
-                for j in 0..4 {
-                    let next = Point {
-                        r: pos.r + dr[j],
-                        c: pos.c + dc[j],
-                    };
-                    if is_out_of_bounds(&next, width, height) || visited.contains(&next) {
-                        continue;
+                let neighbors = vec![
+                    Point { r: pos.r + 1, c: pos.c },
+                    Point { r: pos.r - 1, c: pos.c },
+                    Point { r: pos.r, c: pos.c + 1 },
+                    Point { r: pos.r, c: pos.c - 1 },
+                ];
+                neighbors.iter().for_each(|neighbor| {
+                    if let Some(height) = self.get_height(neighbor) { // checks if point is out-of-bounds
+                        if !visited.contains(neighbor) && calc_height_diff(pos_height, height) <= 1 {
+                            queue.push_back(neighbor.clone());
+                            visited.insert(neighbor.clone());
+                        }
                     }
-                    let height_diff = self.get_height(&next) - self.get_height(&pos);
-                    if height_diff <= 1 {
-                        queue.push_back(next.clone());
-                        visited.insert(next);
-                    }
-                }
+                });
             }
-            step += 1;
+            step += 1
         }
         None
     }
-
-    fn get_lowest_heights(&self) -> Vec<Point> {
-        let mut points: Vec<Point> = vec![];
-        for (i, row) in self.map.iter().enumerate() {
-            for (j, height) in row.iter().enumerate() {
-                if *height == 0 {
-                    points.push(Point { r: i as i32, c: j as i32 });
-                }
-            }
-        }
-        points
-    }
 }
 
-fn new_height_map(input: &str) -> HeightMap {
-    let mut start = Point { r: 0, c: 0 };
-    let mut end = Point { r: 0, c: 0 };
-    let map = input.lines().enumerate().map(|(r, line)| {
-        line.chars().enumerate().map(|(c, char)| {
-            match char {
-                'S' => {
-                    start = Point { r: r as i32, c: c as i32 };
-                    0
-                }
-                'E' => {
-                    end = Point { r: r as i32, c: c as i32 };
-                    26
-                }
-                _ => char as i32 - 'a' as i32
-            }
-        }).collect()
-    }).collect();
-    HeightMap { map, start, end }
+fn climb(from: i32, to: i32) -> i32 {
+    to - from
 }
 
-fn is_out_of_bounds(point: &Point, w: i32, h: i32) -> bool {
-    point.r < 0 || point.r >= h || point.c < 0 || point.c >= w
+fn descend(from: i32, to: i32) -> i32 {
+    from - to
 }
 
 pub fn process_part_1(input: &str) -> String {
-    let hm = new_height_map(input);
-    hm.find_min_path(&hm.start).unwrap().to_string()
+    let hm = HeightMap::from(input);
+    let target_height = hm.get_height(&hm.end).unwrap();
+    let min_distance = hm
+        .find_min_path_to_height(&hm.start, target_height, climb)
+        .unwrap();
+    min_distance.to_string()
 }
 
 pub fn process_part_2(input: &str) -> String {
-    let hm = new_height_map(input);
-    let min_distance = hm.get_lowest_heights()
-        .iter()
-        .filter_map(|point| hm.find_min_path(point))
-        .min()
+    let hm = HeightMap::from(input);
+    let target_height = 0;
+    let min_distance = hm
+        .find_min_path_to_height(&hm.end, target_height, descend)
         .unwrap();
     min_distance.to_string()
 }
@@ -114,13 +107,13 @@ mod tests {
 
     #[test]
     fn part1_small_input() {
-        let file = fs::read_to_string("./../../inputs/12/small_input.txt").unwrap();
+        let file = fs::read_to_string("./../../input/12/small_input.txt").unwrap();
         assert_eq!(process_part_1(&file), "31")
     }
 
     #[test]
     fn part2_small_input() {
-        let file = fs::read_to_string("./../../inputs/12/small_input.txt").unwrap();
+        let file = fs::read_to_string("./../../input/12/small_input.txt").unwrap();
         assert_eq!(process_part_2(&file), "29")
     }
 }
