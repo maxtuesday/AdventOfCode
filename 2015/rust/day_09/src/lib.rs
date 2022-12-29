b6cca7a19093861d6c5560f6188e094fe102e242
maxtuesday <jonesmtuesday@gmail.com>
2022-12-29 17:49:40 -0500

    2015 day 09 - rust - refactor

--- 2015/rust/day_09/src/lib.rs
+++ 2015/rust/day_09/src/lib.rs
@@ -1,5 +1,10 @@
 use std::collections::{HashMap, HashSet};
 
+enum PathCost {
+    MIN,
+    MAX,
+}
+
 fn parse_adjacency_list(input: &str) -> HashMap<String, Vec<(String, i32)>> {
     let mut adj: HashMap<String, Vec<(String, i32)>> = HashMap::new();
     for line in input.lines() {
@@ -21,18 +26,21 @@ fn parse_adjacency_list(input: &str) -> HashMap<String, Vec<(String, i32)>> {
     adj
 }
 
-pub fn process_part_1(input: &str) -> String {
-    let adj = parse_adjacency_list(input);
-
-    let min_path_cost = adj.iter()
+fn find_paths(adj: HashMap<String, Vec<(String, i32)>>, path_cost_type: PathCost) -> Vec<i32> {
+    adj.iter()
         .map(|(start, neighbors)| {
             let mut visited = HashSet::from([start.clone()]);
             let mut path_cost = 0;
             let mut next_neighbors = neighbors.clone();
             loop {
-                let next = next_neighbors.into_iter()
+                let mut next_available = next_neighbors.into_iter()
                     .filter(|node| !visited.contains(node.0.as_str()))
-                    .min_by(|a, b| a.1.cmp(&b.1));
+                    .collect::<Vec<_>>();
+                next_available.sort_by(|a, b| a.1.cmp(&b.1));
+                let next = match path_cost_type {
+                    PathCost::MIN => next_available.first(),
+                    PathCost::MAX => next_available.last(),
+                };
                 if let Some(n) = next {
                     path_cost += n.1;
                     visited.insert(n.0.clone());
@@ -43,38 +51,23 @@ pub fn process_part_1(input: &str) -> String {
             }
             path_cost
         })
-        .min()
-        .unwrap();
+        .collect()
+}
 
-    min_path_cost.to_string()
+pub fn process_part_1(input: &str) -> String {
+    let adj = parse_adjacency_list(input);
+    find_paths(adj, PathCost::MIN).iter()
+        .min()
+        .unwrap()
+        .to_string()
 }
 
 pub fn process_part_2(input: &str) -> String {
     let adj = parse_adjacency_list(input);
-
-    let max_path_cost = adj.iter()
-        .map(|(start, neighbors)| {
-            let mut visited = HashSet::from([start.clone()]);
-            let mut path_cost = 0;
-            let mut next_neighbors = neighbors.clone();
-            loop {
-                let next = next_neighbors.into_iter()
-                    .filter(|node| !visited.contains(node.0.as_str()))
-                    .max_by(|a, b| a.1.cmp(&b.1));
-                if let Some(n) = next {
-                    path_cost += n.1;
-                    visited.insert(n.0.clone());
-                    next_neighbors = adj.get(n.0.as_str()).unwrap().clone();
-                } else {
-                    break;
-                }
-            }
-            path_cost
-        })
+    find_paths(adj, PathCost::MAX).iter()
         .max()
-        .unwrap();
-
-    max_path_cost.to_string()
+        .unwrap()
+        .to_string()
 }
 
 #[cfg(test)]
