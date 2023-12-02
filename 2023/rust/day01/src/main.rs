cc5b1bd3e853bc9ca5edbbc38235a74921ecfa52
maxtuesday <jonesmtuesday@gmail.com>
2023-12-02 16:12:13 -0500

    setup 2023 rust workspace

---
+++ 2023/rust/day01/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("../../../input/day_01.txt");
+    println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
+}
+
+
+fn part1(input: &str) -> &str {
+    input
+}
+
+fn part2(input: &str) -> &str {
+    input
+}
