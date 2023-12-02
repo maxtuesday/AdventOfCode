9b6f44e1fcc0c04ed929371e52636722e3684447
maxtuesday <jonesmtuesday@gmail.com>
2023-12-02 16:30:59 -0500

    solve 2023 day 1 part 1

--- 2023/rust/day01/src/main.rs
+++ 2023/rust/day01/src/main.rs
@@ -1,14 +1,35 @@
 fn main() {
     let input = include_str!("../../../input/day_01.txt");
     println!("Part 1: {}", part1(input));
-    println!("Part 2: {}", part2(input));
 }
 
-
-fn part1(input: &str) -> &str {
+fn part1(input: &str) -> i64 {
     input
+        .lines()
+        .map(|line| {
+            let digits = line
+                .chars()
+                .filter(|char| char.is_digit(10))
+                .collect::<Vec<_>>();
+            let first = digits.first().unwrap();
+            let last = digits.last().unwrap();
+            format!("{}{}", *first, *last).parse::<i64>().unwrap()
+        })
+        .sum()
 }
 
-fn part2(input: &str) -> &str {
-    input
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+";
+
+    #[test]
+    fn test_part1_sample() {
+        assert_eq!(part1(INPUT), 142);
+    }
 }
