921056bb9f2ba77e3ab06c88daa1e5c965855216
Max Jones <jonesmtuesday@gmail.com>
2023-12-03 02:39:38 -0500

    solve 2023 day 2 part 2

--- 2023/rust/day02/src/main.rs
+++ 2023/rust/day02/src/main.rs
@@ -3,6 +3,7 @@ use std::{cmp::max, collections::HashMap};
 fn main() {
     let input = include_str!("../../../input/day_02.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -41,10 +42,8 @@ fn parse(input: &str) -> Vec<Game> {
                 rocks: HashMap::from([(Color::Red, 0), (Color::Green, 0), (Color::Blue, 0)]),
             };
 
-            let sets = rest.split(";");
-            sets.for_each(|set| {
-                let rocks = set.split(",");
-                rocks.for_each(|rock| {
+            rest.split(";").for_each(|set| {
+                set.split(",").for_each(|rock| {
                     let (count, color) = rock.trim().split_once(" ").unwrap();
                     let count = count.parse::<usize>().unwrap();
                     let color = Color::from(color);
@@ -58,8 +57,7 @@ fn parse(input: &str) -> Vec<Game> {
 }
 
 fn part1(input: &str) -> usize {
-    let games = parse(input);
-    games
+    parse(input)
         .iter()
         .filter(|game| {
             game.rocks.get(&Color::Red).unwrap() <= &12
@@ -70,17 +68,30 @@ fn part1(input: &str) -> usize {
         .sum::<usize>()
 }
 
+fn part2(input: &str) -> usize {
+    parse(input)
+        .iter()
+        .map(|game| game.rocks.values().product::<usize>())
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1_sample() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!(part1(input), 8);
+
+    #[test]
+    fn test_part1_sample() {
+        assert_eq!(part1(INPUT), 8);
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        assert_eq!(part2(INPUT), 2286);
     }
 }
