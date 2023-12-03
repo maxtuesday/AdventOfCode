2083cc7e5271aa58280690b28fa15a821177daa0
maxtuesday <jonesmtuesday@gmail.com>
2023-12-02 23:17:48 -0500

    refactor day 1 solution

--- 2023/rust/day01/src/main.rs
+++ 2023/rust/day01/src/main.rs
@@ -4,84 +4,44 @@ fn main() {
     println!("Part 2: {}", part2(input));
 }
 
-struct Digit {
-    idx: usize,
-    digit: u32,
-}
-
-impl From<&(usize, char)> for Digit {
-    fn from(val: &(usize, char)) -> Self {
-        Digit {
-            idx: val.0,
-            digit: val.1.to_digit(10).unwrap(),
-        }
-    }
-}
-
-fn find_first_and_last_digits(line: &str) -> Option<(Digit, Digit)> {
-    let digits = line
-        .chars()
-        .enumerate()
-        .filter(|(_, char)| char.is_digit(10))
-        .collect::<Vec<_>>();
-    Some((Digit::from(digits.first()?), Digit::from(digits.last()?)))
-}
-
-fn part1(input: &str) -> u64 {
+fn part1(input: &str) -> u32 {
     input
-        .lines()
-        .map(|line| {
-            // First part of problem guarentees there is at least 1 digit in each line
-            let (first, last) = find_first_and_last_digits(line).unwrap();
-            format!("{}{}", first.digit, last.digit)
-                .parse::<u64>()
-                .unwrap()
-        })
-        .sum()
+    .lines()
+    .map(|line| {
+        let digits = line.chars()
+            .filter_map(|c| c.to_digit(10))
+            .collect::<Vec<_>>();
+        // First part of problem guarentees there is at least 1 digit in each line
+        10*digits.first().unwrap() + digits.last().unwrap()
+    })
+    .sum()
 }
 
-fn part2(input: &str) -> u64 {
-    // Could do this with some regex index matching
+fn part2(input: &str) -> u32 {
     let nums = vec![
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
-    input
-        .lines()
-        .map(|line| {
-            let mut first_num = Digit {
-                idx: line.len() + 1,
-                digit: 0,
-            };
-            let mut last_num = Digit { idx: 0, digit: 0 };
-            if let Some((first, last)) = find_first_and_last_digits(line) {
-                first_num = first;
-                last_num = last;
-            }
-
-            for (idx, n) in nums.iter().enumerate() {
-                if let Some(first) = line.find(n) {
-                    if first < first_num.idx {
-                        first_num.digit = (idx + 1) as u32;
-                        first_num.idx = first;
+    input.lines().map(|line| {
+        let digits = line.chars().enumerate()
+            .filter_map(|(i,c)| {
+                c.to_digit(10).or_else(|| {
+                    for (idx, n) in nums.iter().enumerate() {
+                        if line[i..].starts_with(n) {
+                            // We will only start with one of the number words
+                            return Some((idx + 1) as u32);
+                        }
                     }
-                };
-
-                if let Some(last) = line.rfind(n) {
-                    if last > last_num.idx {
-                        last_num.digit = (idx + 1) as u32;
-                        last_num.idx = last;
-                    }
-                }
-            }
-            format!("{}{}", first_num.digit, last_num.digit)
-                .parse::<u64>()
-                .unwrap()
-        })
-        .sum()
+                    None
+                })
+            })
+            .collect::<Vec<_>>();
+        10*digits.first().unwrap() + digits.last().unwrap()
+    })
+    .sum()
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
 
     #[test]
