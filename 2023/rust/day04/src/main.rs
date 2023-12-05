5123461d4bc02e36ae3177f9de3b1eba895807c7
maxtuesday <jonesmtuesday@gmail.com>
2023-12-05 17:34:27 -0500

    refactor 2023 day 4 part 1

--- 2023/rust/day04/src/main.rs
+++ 2023/rust/day04/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     let input = include_str!("../../../input/day_04.txt");
@@ -14,8 +14,7 @@ fn parse_nums(nums: &str) -> HashSet<u32> {
 
 struct Card {
     id: u32,
-    left: HashSet<u32>,
-    right: HashSet<u32>,
+    matches: u32,
 }
 
 impl From<&str> for Card {
@@ -34,41 +33,29 @@ impl From<&str> for Card {
             .expect("number lists should be separated with |");
         let left = parse_nums(left);
         let right = parse_nums(right);
-        Card { id, left, right }
+        let matches = left.intersection(&right).collect::<Vec<_>>().len();
+        let matches = if matches > 0 { 1 << matches - 1 } else { 0 };
+        Card { id, matches }
     }
 }
 
 fn part1(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|line| {
-            let card = Card::from(line);
-            let matches = card
-                .left
-                .intersection(&card.right)
-                .collect::<Vec<_>>()
-                .len();
-            if matches > 0 {
-                1 << matches - 1
-            } else {
-                0
-            }
-        })
-        .sum()
+    input.lines().map(|line| Card::from(line).matches).sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_part1_sample() {
-        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    const INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-        assert_eq!(part1(input), 13);
+
+    #[test]
+    fn test_part1_sample() {
+        assert_eq!(part1(INPUT), 13);
     }
 }
