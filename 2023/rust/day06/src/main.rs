7b6af00645386be3388a762a69650f61803128ce
maxtuesday <jonesmtuesday@gmail.com>
2023-12-09 03:29:15 -0500

    solve 2023 day 6 part 2

--- 2023/rust/day06/src/main.rs
+++ 2023/rust/day06/src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
     let input = include_str!("../../../input/day_06.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
-fn parse(input: &str) -> Vec<(u32, u32)> {
+fn parse(input: &str) -> Vec<(usize, usize)> {
     let nums = input
         .lines()
         .map(|line| {
             line.split(" ")
-                .filter_map(|word| word.parse::<u32>().ok())
+                .filter_map(|word| word.parse::<usize>().ok())
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
@@ -17,19 +18,48 @@ fn parse(input: &str) -> Vec<(u32, u32)> {
     times.into_iter().zip(distances).collect()
 }
 
+fn travel_distance(hold_time: usize, time: usize) -> usize {
+    (time - hold_time) * hold_time
+}
+
 fn part1(input: &str) -> usize {
     let pairs = parse(input);
     pairs
         .iter()
         .map(|&(time, distance)| {
             (1..time)
-                .filter(|&t| {
-                    let rem = time - t;
-                    t * rem > distance
-                })
+                .filter(|&t| travel_distance(t, time) > distance)
                 .count()
         })
         .product()
+}
+
+fn part2(input: &str) -> usize {
+    let (time, distance) = parse(input)
+        .into_iter()
+        .fold((String::new(), String::new()), |acc, x| {
+            (format!("{}{}", acc.0, x.0), format!("{}{}", acc.1, x.1))
+        });
+    let time = time.parse::<usize>().expect("must be number");
+    let distance = distance.parse::<usize>().expect("must be number");
+
+    // We need to find the first time we beat the distance record
+    // and the last time we beat the distance record:
+    // Binary Search!
+
+    // Since we are using a difference value for the search_by,
+    // it will return Err since that value is not actually in the slice
+    let time_range = Vec::from_iter(0..=time);
+    let left = time_range
+        .as_slice()
+        .binary_search_by(|&x| travel_distance(x, time).cmp(&distance))
+        .unwrap_err();
+    let right = time_range
+        .as_slice()
+        .binary_search_by(|&x| distance.cmp(&travel_distance(x, time)))
+        .unwrap_err();
+
+    right - left
 }
 
 #[cfg(test)]
@@ -42,5 +72,10 @@ Distance:  9  40  200";
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT), 288);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 71503);
     }
 }
