7b7e073c9ce2e30ea654cf38e5662fcf3adc4189
maxtuesday <jonesmtuesday@gmail.com>
2023-12-19 22:11:53 -0600

    solve 2023 day 13 part 2

--- 2023/rust/day13/src/main.rs
+++ 2023/rust/day13/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let input = include_str!("../../../input/day13.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 type Grid = Vec<Vec<char>>;
@@ -29,14 +30,23 @@ fn transpose(grid: &Grid) -> Grid {
     new_grid
 }
 
-fn check_reflection(grid: &Grid, mut l: usize, mut r: usize) -> bool {
+fn count_differences(left: &Vec<char>, right: &Vec<char>) -> usize {
+    assert!(left.len() == right.len());
+    let mut count = 0;
+    for i in 0..left.len() {
+        if left[i] != right[i] {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+fn check_perfect_reflection(grid: &Grid, mut l: usize, mut r: usize) -> bool {
     loop {
         let left = grid.get(l);
         let right = grid.get(r);
         match (left, right) {
-            (None, None) => return true,
-            (None, Some(_)) => return true,
-            (Some(_), None) => return true,
+            (None, None) | (None, Some(_)) | (Some(_), None) => return true,
             (Some(left), Some(right)) => {
                 if left != right {
                     return false;
@@ -52,30 +62,82 @@ fn check_reflection(grid: &Grid, mut l: usize, mut r: usize) -> bool {
     }
 }
 
-fn first_reflection(grid: &Grid) -> Option<usize> {
+fn check_smudge_reflection(grid: &Grid, mut l: usize, mut r: usize) -> bool {
+    let mut smudges = 0;
+    loop {
+        let left = grid.get(l);
+        let right = grid.get(r);
+        match (left, right) {
+            (None, None) | (None, Some(_)) | (Some(_), None) => break,
+            (Some(left), Some(right)) => {
+                let difference_count = count_differences(left, right);
+                if difference_count > 1 {
+                    return false;
+                }
+                smudges += difference_count;
+            }
+        }
+        // will the next iteration make l negative?
+        if l == 0 {
+            break;
+        }
+        l -= 1;
+        r += 1;
+    }
+    smudges == 1
+}
+
+fn first_perfect_reflection(grid: &Grid) -> Option<usize> {
     let reflection_indexes = (0..grid.len() - 1)
-        .filter(|&i| check_reflection(grid, i, i + 1))
+        .filter(|&i| check_perfect_reflection(grid, i, i + 1))
         .collect::<Vec<_>>();
     // There should be only 0 or 1 indexes found
     assert!(reflection_indexes.len() <= 1);
     reflection_indexes.first().copied()
 }
 
-fn find_reflection(grid: &Grid) -> Reflection {
-    if let Some(row) = first_reflection(grid) {
+fn first_smudge_reflection(grid: &Grid) -> Option<usize> {
+    let reflection_indexes = (0..grid.len() - 1)
+        .filter(|&i| check_smudge_reflection(grid, i, i + 1))
+        .collect::<Vec<_>>();
+    assert!(reflection_indexes.len() <= 1);
+    reflection_indexes.first().copied()
+}
+
+fn find_perfect_reflection(grid: &Grid) -> Reflection {
+    if let Some(row) = first_perfect_reflection(grid) {
         return Reflection::Row(row + 1);
     }
-    if let Some(col) = first_reflection(&transpose(grid)) {
+    if let Some(col) = first_perfect_reflection(&transpose(grid)) {
         return Reflection::Col(col + 1);
     }
+    unreachable!("did not find a reflection");
+}
 
+fn find_smudge_reflection(grid: &Grid) -> Reflection {
+    if let Some(row) = first_smudge_reflection(grid) {
+        return Reflection::Row(row + 1);
+    }
+    if let Some(col) = first_smudge_reflection(&transpose(grid)) {
+        return Reflection::Col(col + 1);
+    }
     unreachable!("did not find a reflection");
 }
 
 fn part1(input: &str) -> usize {
     parse(input)
         .into_iter()
-        .map(|grid| match find_reflection(&grid) {
+        .map(|grid| match find_perfect_reflection(&grid) {
+            Reflection::Row(val) => val * 100,
+            Reflection::Col(val) => val,
+        })
+        .sum()
+}
+
+fn part2(input: &str) ->usize {
+    parse(input)
+        .into_iter()
+        .map(|grid| match find_smudge_reflection(&grid) {
             Reflection::Row(val) => val * 100,
             Reflection::Col(val) => val,
         })
@@ -129,5 +191,10 @@ mod tests {
 ..##..###
 #....#..#";
         assert_eq!(part1(input), 400);
+    }
+
+    #[test]
+    fn test_part2_ex() {
+        assert_eq!(part2(INPUT), 400);
     }
 }
