c9def806a89aaa05e662b959b9ce6b23789c0505
maxtuesday <jonesmtuesday@gmail.com>
2024-01-16 23:50:13 -0500

    solve 2023 day 17 part 1

---
+++ 2023/rust/day17/src/main.rs
@@ -0,0 +1,210 @@
+use std::collections::{BinaryHeap, HashSet};
+
+fn main() {
+    let input = include_str!("../../../input/day17.txt");
+    println!("Part 1: {}", part1(input));
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+fn next_directions(direction: &Direction) -> Vec<Direction> {
+    match direction {
+        Direction::Up | Direction::Down => {
+            vec![direction.clone(), Direction::Left, Direction::Right]
+        }
+        Direction::Left | Direction::Right => {
+            vec![direction.clone(), Direction::Up, Direction::Down]
+        }
+    }
+}
+
+type Grid = Vec<Vec<u32>>;
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+struct Step {
+    pos: Pos,
+    direction: Direction,
+    steps: u8,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct State {
+    step: Step,
+    cost: u32,
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+struct Graph {
+    grid: Grid,
+}
+
+impl Graph {
+    fn from(s: &str) -> Self {
+        let grid = s
+            .lines()
+            .map(|line| line.chars().filter_map(|c| c.to_digit(10)).collect())
+            .collect();
+        Self { grid }
+    }
+
+    fn next_pos(&self, pos: &Pos, direction: &Direction) -> Option<Pos> {
+        let (x, y) = match direction {
+            Direction::Up => {
+                let Some(y) = pos.y.checked_sub(1) else {
+                    return None;
+                };
+                (pos.x, y)
+            }
+            Direction::Down => {
+                let y = pos.y + 1;
+                if y >= self.grid.len() {
+                    return None;
+                }
+                (pos.x, y)
+            }
+            Direction::Left => {
+                let Some(x) = pos.x.checked_sub(1) else {
+                    return None;
+                };
+                (x, pos.y)
+            }
+            Direction::Right => {
+                let x = pos.x + 1;
+                if x >= self.grid[0].len() {
+                    return None;
+                }
+                (x, pos.y)
+            }
+        };
+        Some(Pos { x, y })
+    }
+
+    fn next_step(&self, state: &State, direction: Direction) -> Option<Step> {
+        let Some(pos) = self.next_pos(&state.step.pos, &direction) else {
+            return None;
+        };
+        let steps = if state.step.direction != direction {
+            1
+        } else {
+            state.step.steps + 1
+        };
+        let next = Step {
+            pos,
+            direction,
+            steps,
+        };
+        if next.steps > 3 {
+            return None;
+        }
+        Some(next)
+    }
+
+    fn successors(&self, state: &State) -> Vec<Step> {
+        next_directions(&state.step.direction)
+            .into_iter()
+            .filter_map(|dir| self.next_step(&state, dir))
+            .collect()
+    }
+}
+
+fn get_min_heatloss(graph: &Graph) -> Option<u32> {
+    let start_right = State {
+        step: Step {
+            pos: Pos { x: 1, y: 0 },
+            direction: Direction::Right,
+            steps: 1,
+        },
+        cost: graph.grid[0][1],
+    };
+    let start_down = State {
+        step: Step {
+            pos: Pos { x: 0, y: 1 },
+            direction: Direction::Down,
+            steps: 1,
+        },
+        cost: graph.grid[1][0],
+    };
+
+    let mut visited: HashSet<Step> =
+        HashSet::from([start_right.step.clone(), start_down.step.clone()]);
+
+    let mut heap = BinaryHeap::new();
+    heap.push(start_right);
+    heap.push(start_down);
+
+    let goal = Pos {
+        x: graph.grid[0].len() - 1,
+        y: graph.grid.len() - 1,
+    };
+
+    while let Some(state) = heap.pop() {
+        if state.step.pos == goal {
+            return Some(state.cost);
+        }
+
+        for next_step in graph.successors(&state) {
+            let cost = graph.grid[next_step.pos.y][next_step.pos.x] + state.cost;
+
+            if !visited.contains(&next_step) {
+                visited.insert(next_step.clone());
+                heap.push(State {
+                    step: next_step,
+                    cost,
+                });
+            }
+        }
+    }
+    None
+}
+
+fn part1(input: &str) -> u32 {
+    let graph = Graph::from(input);
+    get_min_heatloss(&graph).expect("did not find goal")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533";
+
+    #[test]
+    fn test_part1_ex() {
+        assert_eq!(part1(INPUT), 102);
+    }
+}
