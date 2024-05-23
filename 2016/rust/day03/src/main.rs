8bf31627cba59253a816161a76c73f68213b5cd8
maxtuesday <jonesmtuesday@gmail.com>
2024-05-23 18:56:34 -0400

    solve 2016 day 3 part 2

--- 2016/rust/day03/src/main.rs
+++ 2016/rust/day03/src/main.rs
@@ -1,26 +1,48 @@
 fn main() {
     let input = include_str!("../../../input/day03.txt");
     println!("Part 1: {}", part1(input));
+    println!("Part 2: {}", part2(input));
 }
 
 fn is_valid_tri(a: u32, b: u32, c: u32) -> bool {
     return a + b > c && b + c > a && c + a > b;
 }
 
-fn part1(input: &str) -> usize {
+fn parse(input: &str) -> Vec<Vec<u32>> {
     input
         .lines()
-        .filter(|line| {
+        .map(|line| {
             let sides = line
                 .split(' ')
                 .filter_map(|num| num.trim().parse::<u32>().ok())
                 .collect::<Vec<u32>>();
-            
-            assert!(sides.len() == 3, "sides does not have 3 numbers");
 
-            is_valid_tri(sides[0], sides[1], sides[2])
+            assert!(sides.len() == 3, "sides does not have 3 numbers");
+            sides
         })
+        .collect()
+}
+
+fn part1(input: &str) -> usize {
+    parse(input)
+        .iter()
+        .filter(|sides| is_valid_tri(sides[0], sides[1], sides[2]))
         .count()
+}
+
+fn part2(input: &str) -> usize {
+    let sides = parse(input);
+
+    let mut count = 0;
+    for r in (0..sides.len()).step_by(3) {
+        for c in 0..3 {
+            if is_valid_tri(sides[r][c], sides[r + 1][c], sides[r + 2][c]) {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
@@ -29,7 +51,13 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let input = "  10 25 5";
-        assert_eq!(part1(input), 0);
+        let input = include_str!("../../../input/day03.txt");
+        assert_eq!(part1(input), 993);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../../../input/day03.txt");
+        assert_eq!(part2(input), 1849);
     }
 }
