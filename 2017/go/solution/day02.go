736afc096ba30ae67014787da7fd541dae5cb914
maxtuesday <jonesmtuesday@gmail.com>
2024-08-07 00:24:40 -0400

    solve 2017 day 2 part 2

--- 2017/go/solution/day02.go
+++ 2017/go/solution/day02.go
@@ -34,5 +34,35 @@ func (d Day02) Part1(input string) string {
 }
 
 func (d Day02) Part2(input string) string {
-	return ""
+	sum := 0
+	for _, line := range strings.Split(input, "\n") {
+		numsRaw := strings.Fields(line)
+		nums := make([]int, len(numsRaw))
+		for i := range numsRaw {
+			n, err := strconv.Atoi(numsRaw[i])
+			if err != nil {
+				panic(err)
+			}
+			nums[i] = n
+		}
+		sum += getDivision(nums)
+	}
+	return fmt.Sprintf("%d", sum)
+}
+
+func getDivision(nums []int) int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			// check if numbers evenly divide each other
+			a, b := nums[i], nums[j]
+			if a < b {
+				// swap so a is larger
+				a, b = b, a
+			}
+			if a%b == 0 {
+				return a / b
+			}
+		}
+	}
+	panic("unreachable")
 }
--- 2017/go/solution/day02_test.go
+++ 2017/go/solution/day02_test.go
@@ -24,3 +24,26 @@ func TestDay02_Part1(t *testing.T) {
 		}
 	}
 }
+
+func TestDay02_Part2(t *testing.T) {
+	type TestCase struct {
+		input    string
+		expected string
+	}
+
+	tts := []TestCase{
+		{
+			input: `5 9 2 8
+9 4 7 3
+3 8 6 5`,
+			expected: "9",
+		},
+	}
+
+	for _, tt := range tts {
+		actual := Day02{}.Part2(tt.input)
+		if actual != tt.expected {
+			t.Errorf("Expected: %s; Got: %s", tt.expected, actual)
+		}
+	}
+}
