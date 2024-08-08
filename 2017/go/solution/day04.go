7050d8cfbfd103391ede82c7f62b9235dd775eb9
Max Jones <jonesmtuesday@gmail.com>
2024-08-07 20:49:22 -0400

    solve 2017 day 4 part 2

--- 2017/go/solution/day04.go
+++ 2017/go/solution/day04.go
@@ -34,5 +34,35 @@ func isValid(words []string) bool {
 }
 
 func (d Day04) Part2(input string) string {
-	return ""
+	count := 0
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		words := strings.Fields(line)
+		if isValidV2(words) {
+			count++
+		}
+	}
+
+	return fmt.Sprintf("%d", count)
+}
+
+func isValidV2(words []string) bool {
+	seen := map[[26]byte]struct{}{}
+	for _, word := range words {
+		key := makeKey(word)
+		if _, ok := seen[key]; ok {
+			return false
+		}
+		seen[key] = struct{}{}
+	}
+	return true
+}
+
+func makeKey(word string) [26]byte {
+	key := [26]byte{}
+	for i := range word {
+		key[int(word[i]-'a')]++
+	}
+	return key
 }
--- 2017/go/solution/day04_test.go
+++ 2017/go/solution/day04_test.go
@@ -30,3 +30,40 @@ func TestDay04_Part1(t *testing.T) {
 		}
 	}
 }
+
+func TestDay04_Part2(t *testing.T) {
+	type TestCase struct {
+		input    string
+		expected string
+	}
+
+	tts := []TestCase{
+		{
+			input:    `abcde fghij`,
+			expected: "1",
+		},
+		{
+			input:    `abcde xyz ecdab`,
+			expected: "0",
+		},
+		{
+			input:    `a ab abc abd abf abj`,
+			expected: "1",
+		},
+		{
+			input:    `iiii oiii ooii oooi oooo`,
+			expected: "1",
+		},
+		{
+			input:    `oiii ioii iioi iiio`,
+			expected: "0",
+		},
+	}
+
+	for _, tt := range tts {
+		actual := Day04{}.Part2(tt.input)
+		if actual != tt.expected {
+			t.Errorf("Expected: %s; Got: %s", tt.expected, actual)
+		}
+	}
+}
