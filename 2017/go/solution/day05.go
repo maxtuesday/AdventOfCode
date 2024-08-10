d4bd2c974387906a02b17900e9915e51725b29a8
maxtuesday <jonesmtuesday@gmail.com>
2024-08-09 23:47:39 -0400

    solve 2017 day 5 part 2

--- 2017/go/solution/day05.go
+++ 2017/go/solution/day05.go
@@ -9,7 +9,7 @@ import (
 type Day05 struct {
 }
 
-func (d Day05) Part1(input string) string {
+func parse(input string) []int {
 	tokens := strings.Split(input, "\n")
 	jumps := make([]int, len(tokens))
 	for i := range tokens {
@@ -19,6 +19,11 @@ func (d Day05) Part1(input string) string {
 		}
 		jumps[i] = jump
 	}
+	return jumps
+}
+
+func (d Day05) Part1(input string) string {
+	jumps := parse(input)
 
 	// simulate jumps
 	idx := 0
@@ -35,5 +40,22 @@ func (d Day05) Part1(input string) string {
 }
 
 func (d Day05) Part2(input string) string {
-	return ""
+	jumps := parse(input)
+
+	// simulate jumps
+	idx := 0
+	steps := 0
+	for 0 <= idx && idx < len(jumps) {
+		// figure out next jump
+		nextIdx := idx + jumps[idx]
+		if jumps[idx] >= 3 {
+			jumps[idx]--
+		} else {
+			jumps[idx]++
+		}
+		idx = nextIdx
+		steps++
+	}
+
+	return fmt.Sprintf("%d", steps)
 }
