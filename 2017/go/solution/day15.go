2db75401feeca7e45bd4c9a8572f3a0d279823bb
maxtuesday <jonesmtuesday@gmail.com>
2024-09-14 19:04:37 -0400

    solve 2017 day 15 part 2

--- 2017/go/solution/day15.go
+++ 2017/go/solution/day15.go
@@ -21,10 +21,15 @@ func getStartingValue(line string) int {
 	return n
 }
 
-func (d Day15) Part1(input string) string {
+func (d Day15) parse(input string) (int, int) {
 	lines := strings.Split(input, "\n")
 	a := getStartingValue(lines[0])
 	b := getStartingValue(lines[1])
+	return a, b
+}
+
+func (d Day15) Part1(input string) string {
+	a, b := d.parse(input)
 
 	count := 0
 	for i := 0; i < 40_000_000; i++ {
@@ -41,6 +46,47 @@ func (d Day15) Part1(input string) string {
 	return fmt.Sprintf("%d", count)
 }
 
+type Generator struct {
+	n      int
+	factor int
+	mod    int
+}
+
+func (g *Generator) Next() int {
+	for {
+		g.n = (g.n * g.factor) % Div
+		if g.n%g.mod == 0 {
+			return g.n
+		}
+	}
+}
+
+// Can improve time with go routines
 func (d Day15) Part2(input string) string {
-	return ""
+	an, bn := d.parse(input)
+
+	genA := Generator{
+		n:      an,
+		factor: FactorA,
+		mod:    4,
+	}
+	genB := Generator{
+		n:      bn,
+		factor: FactorB,
+		mod:    8,
+	}
+
+	count := 0
+	for i := 0; i < 5_000_000; i++ {
+		a := genA.Next()
+		b := genB.Next()
+
+		aa := (uint32(a) << 16) >> 16
+		bb := (uint32(b) << 16) >> 16
+		if aa == bb {
+			count++
+		}
+	}
+
+	return fmt.Sprintf("%d", count)
 }
--- 2017/go/solution/day15_test.go
+++ 2017/go/solution/day15_test.go
@@ -23,3 +23,25 @@ Generator B starts with 8921`,
 		}
 	}
 }
+
+func TestDay15_Part2(t *testing.T) {
+	type TestCase struct {
+		input    string
+		expected string
+	}
+
+	tts := []TestCase{
+		{
+			input: `Generator A starts with 65
+Generator B starts with 8921`,
+			expected: "309",
+		},
+	}
+
+	for _, tt := range tts {
+		actual := Day15{}.Part2(tt.input)
+		if actual != tt.expected {
+			t.Errorf("Expected: %s; Got: %s", tt.expected, actual)
+		}
+	}
+}
