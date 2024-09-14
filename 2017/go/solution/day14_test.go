730044ac5e8c25098ba4ed4e0d311b5d8668b378
maxtuesday <jonesmtuesday@gmail.com>
2024-09-14 16:10:22 -0400

    solve 2017 day 14 part 2

--- 2017/go/solution/day14_test.go
+++ 2017/go/solution/day14_test.go
@@ -22,3 +22,24 @@ func TestDay14_Part1(t *testing.T) {
 		}
 	}
 }
+
+func TestDay14_Part2(t *testing.T) {
+	type TestCase struct {
+		input    string
+		expected string
+	}
+
+	tts := []TestCase{
+		{
+			input:    "flqrgnkx",
+			expected: "1242",
+		},
+	}
+
+	for _, tt := range tts {
+		actual := Day14{}.Part2(tt.input)
+		if actual != tt.expected {
+			t.Errorf("Expected: %s; Got: %s", tt.expected, actual)
+		}
+	}
+}
