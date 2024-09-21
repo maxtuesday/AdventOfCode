60c4c57a746f19d475f24454243e4513b4ccc55e
Max Jones <jonesmtuesday@gmail.com>
2024-09-20 20:10:15 -0400

    solve 2017 day 17 part 2

--- 2017/go/solution/day17.go
+++ 2017/go/solution/day17.go
@@ -9,21 +9,30 @@ type Day17 struct {
 }
 
 func (d Day17) Part1(input string) string {
-	steps, _ := strconv.Atoi(input)
+	step, _ := strconv.Atoi(input)
 
 	index := 0
 	buffer := []int{0}
 	for i := 1; i <= 2017; i++ {
-		index += steps
-		index %= len(buffer)
-		buffer = append(buffer[:index+1], buffer[index:]...)
-		buffer[index+1] = i // insert after
-		index += 1
+		index = ((index + step) % i) + 1
+		buffer = append(buffer[:index], buffer[index-1:]...)
+		buffer[index] = i // insert after
 	}
 
 	return fmt.Sprintf("%d", buffer[(index+1)%len(buffer)])
 }
 
 func (d Day17) Part2(input string) string {
-	return ""
+	step, _ := strconv.Atoi(input)
+
+	index := 0
+	nextToZero := 0
+	for i := 1; i <= 50_000_000; i++ {
+		index = ((index + step) % i) + 1
+		if index == 1 {
+			nextToZero = i
+		}
+	}
+
+	return fmt.Sprintf("%d", nextToZero)
 }
