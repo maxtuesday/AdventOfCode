42f831f6e9699ab461c8669f77986c5d4bca7a48
Max Jones <jonesmtuesday@gmail.com>
2024-12-03 17:57:04 -0500

    [rust]: solve 2024 day 2 part 2

--- 2024/rust/src/day02.rs
+++ 2024/rust/src/day02.rs
@@ -8,20 +8,24 @@ struct Report {
 
 impl Report {
     fn is_safe(&self) -> bool {
-        let diffs = self
-            .levels
-            .windows(2)
-            .map(|window| window[0] - window[1])
-            .collect::<Vec<i32>>();
+        is_safe(&self.levels)
+    }
 
-        // - all increasing or all decreasing
-        let all_increasing = diffs.iter().all(|diff| *diff > 0);
-        let all_decreasing = diffs.iter().all(|diff| *diff < 0);
+    fn is_tolerable(&self) -> bool {
+        if self.is_safe() {
+            return true;
+        }
 
-        // - two adj levels differ by at least 1 and at most 3
-        let invalid_differ = diffs.iter().any(|diff | diff.abs() < 1 || diff.abs() > 3);
-
-        (all_increasing || all_decreasing) && !invalid_differ
+        // check if removing one level makes this report safe
+        let size = self.levels.len();
+        for i in 0..size {
+            let mut levels = self.levels.clone();
+            levels.remove(i);
+            if is_safe(&levels) {
+                return true;
+            }
+        }
+        return false;
     }
 }
 
@@ -38,6 +42,22 @@ fn parse(input: &str) -> Vec<Report> {
         .collect()
 }
 
+fn is_safe(levels: &Vec<i32>) -> bool {
+    let diffs = levels
+        .windows(2)
+        .map(|window| window[0] - window[1])
+        .collect::<Vec<i32>>();
+
+    // - all increasing or all decreasing
+    let all_increasing = diffs.iter().all(|diff| *diff > 0);
+    let all_decreasing = diffs.iter().all(|diff| *diff < 0);
+
+    // - two adj levels differ by at least 1 and at most 3
+    let invalid_differ = diffs.iter().any(|diff| diff.abs() < 1 || diff.abs() > 3);
+
+    (all_increasing || all_decreasing) && !invalid_differ
+}
+
 impl Solution for Day02 {
     fn part1(&self, input: &str) -> String {
         let reports = parse(input);
@@ -46,6 +66,11 @@ impl Solution for Day02 {
     }
 
     fn part2(&self, input: &str) -> String {
-        todo!("Part 2")
+        let reports = parse(input);
+        let safe_reports = reports
+            .iter()
+            .filter(|report| report.is_tolerable())
+            .count();
+        format!("{safe_reports}")
     }
 }
