10cc338c973080ce360cf69b91b9794465ab6659
Max Jones <jonesmtuesday@gmail.com>
2024-12-09 02:01:25 -0500

    [rust]: solve 2024 day 5 part 2

--- 2024/rust/src/day05.rs
+++ 2024/rust/src/day05.rs
@@ -1,30 +1,29 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    borrow::Cow,
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
 use crate::solution::Solution;
 
 pub struct Day05;
 
-#[derive(Debug)]
-struct OrderingRules {
-    rules: HashMap<u32, HashSet<u32>>,
-}
+type OrderingRules = HashMap<u32, HashSet<u32>>;
 
-impl OrderingRules {
-    fn from(lines: Vec<&str>) -> Self {
-        let mut rules: HashMap<u32, HashSet<u32>> = HashMap::new();
-        lines.iter().for_each(|line| {
-            let (x, y) = line.split_once("|").expect("rule should be in format X|Y");
-            let x = x.parse::<u32>().expect("should be valid integer");
-            let y = y.parse::<u32>().expect("should be valid integer");
-            rules
-                .entry(x)
-                .and_modify(|val| {
-                    val.insert(y);
-                })
-                .or_insert(HashSet::from([y]));
-        });
-        Self { rules }
-    }
+fn ordering_rules_from_lines(lines: Vec<&str>) -> OrderingRules {
+    let mut rules: HashMap<u32, HashSet<u32>> = HashMap::new();
+    lines.iter().for_each(|line| {
+        let (x, y) = line.split_once("|").expect("rule should be in format X|Y");
+        let x = x.parse::<u32>().expect("should be valid integer");
+        let y = y.parse::<u32>().expect("should be valid integer");
+        rules
+            .entry(x)
+            .and_modify(|val| {
+                val.insert(y);
+            })
+            .or_insert(HashSet::from([y]));
+    });
+    rules
 }
 
 fn parse(input: &str) -> (OrderingRules, Vec<Vec<u32>>) {
@@ -37,10 +36,11 @@ fn parse(input: &str) -> (OrderingRules, Vec<Vec<u32>>) {
 
     let (rules, updates) = lines.split_at(split_point);
 
-    let rules = OrderingRules::from(rules.to_vec());
-    let mut updates= updates.iter();
+    let rules = ordering_rules_from_lines(rules.to_vec());
+    let mut updates = updates.iter();
     updates.next(); // pop empty line
-    let updates = updates.map(|line| {
+    let updates = updates
+        .map(|line| {
             line.split(",")
                 .filter_map(|num| num.parse::<u32>().ok())
                 .collect()
@@ -54,9 +54,8 @@ fn is_valid(update: &Vec<u32>, ordering_rules: &OrderingRules) -> bool {
     let mut seen: HashSet<u32> = HashSet::new();
     for page in update {
         // get all the pages that should be printed after this page from the ordering_rules
-        if let Some(after) = ordering_rules.rules.get(page) {
-            let intersection_count = seen.intersection(after).count();
-            if intersection_count > 0 {
+        if let Some(after) = ordering_rules.get(page) {
+            if seen.intersection(after).count() > 0 {
                 return false;
             }
         }
@@ -65,21 +64,59 @@ fn is_valid(update: &Vec<u32>, ordering_rules: &OrderingRules) -> bool {
     true
 }
 
+fn fix_update(mut update: Vec<u32>, ordering_rules: &OrderingRules) -> Vec<u32> {
+    // We can sort the list based on ordering rules
+    update.sort_by(|a, b| {
+        // check if b is in a_next
+        let a_next = ordering_rules
+            .get(a)
+            .map(Cow::Borrowed)
+            .unwrap_or_else(|| Cow::Owned(HashSet::new()));
+        if a_next.contains(b) {
+            // b should come after a
+            return Ordering::Less;
+        }
+
+        // check if a is in b_next
+        let b_next = ordering_rules
+            .get(b)
+            .map(Cow::Borrowed)
+            .unwrap_or_else(|| Cow::Owned(HashSet::new()));
+        if b_next.contains(a) {
+            // a should come after b
+            return Ordering::Greater;
+        }
+
+        unimplemented!("If we reach this, then we have ordering rules that are transitive")
+    });
+    update
+}
+
 impl Solution for Day05 {
     fn part1(&self, input: &str) -> String {
         let (ordering_rules, updates) = parse(input);
 
-        let sum = updates.iter().filter(|update| {
-            is_valid(update, &ordering_rules)
-        }).map(|update| {
-            let l = update.len();
-            update[l/2]
-        }).sum::<u32>();
+        let sum = updates
+            .iter()
+            .filter(|update| is_valid(update, &ordering_rules))
+            .map(|update| update[update.len() / 2])
+            .sum::<u32>();
 
         format!("{sum}")
     }
 
     fn part2(&self, input: &str) -> String {
-        todo!()
+        let (ordering_rules, updates) = parse(input);
+
+        let sum = updates
+            .into_iter()
+            .filter(|update| !is_valid(update, &ordering_rules))
+            .map(|update| {
+                let new_order = fix_update(update, &ordering_rules);
+                new_order[new_order.len() / 2]
+            })
+            .sum::<u32>();
+
+        format!("{sum}")
     }
 }
