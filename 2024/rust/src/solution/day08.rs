11e177353ea77e7efb3d063e343a766d66ecb67d
Max Jones <jonesmtuesday@gmail.com>
2024-12-14 01:17:19 -0500

    [rust]: solve 2024 day 8 part 2

--- 2024/rust/src/solution/day08.rs
+++ 2024/rust/src/solution/day08.rs
@@ -4,7 +4,7 @@ use super::Solution;
 
 pub struct Day08;
 
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
 struct Pos {
     r: i32,
     c: i32,
@@ -16,7 +16,13 @@ struct Antenna {
     pos: Pos,
 }
 
-fn parse(input: &str) -> (Vec<Antenna>, (i32, i32)) {
+struct Grid {
+    antannea: Vec<Antenna>,
+    r_bounds: i32,
+    c_bounds: i32,
+}
+
+fn parse(input: &str) -> Grid {
     let r_bounds = input.lines().count();
     let c_bounds = input
         .lines()
@@ -44,7 +50,11 @@ fn parse(input: &str) -> (Vec<Antenna>, (i32, i32)) {
         })
         .collect();
 
-    (antannea, (r_bounds as i32, c_bounds as i32))
+    Grid {
+        antannea,
+        r_bounds: r_bounds as i32,
+        c_bounds: c_bounds as i32,
+    }
 }
 
 fn input_as_grid(input: &str) -> Vec<Vec<char>> {
@@ -82,68 +92,92 @@ fn is_oob(p: &Pos, r_bounds: i32, c_bounds: i32) -> bool {
     p.r < 0 || r_bounds <= p.r || p.c < 0 || c_bounds <= p.c
 }
 
-impl Solution for Day08 {
-    fn part1(&self, input: &str) -> String {
-        let (antennae, (r_bounds, c_bounds)) = parse(input);
+fn create_antinodes(grid: &Grid, harmonics: bool) -> HashSet<Pos> {
+    let mut antinode_locations: HashSet<Pos> = HashSet::new();
 
-        // Group antennas by frequency
-        let frequencies = antennae
+    // Group antennas by frequency
+    let frequencies = grid
+        .antannea
+        .iter()
+        .map(|antenna| antenna.frequency)
+        .collect::<HashSet<char>>()
+        .into_iter()
+        .collect::<Vec<char>>();
+
+    for frequency in frequencies {
+        let group = grid
+            .antannea
             .iter()
-            .map(|antenna| antenna.frequency)
-            .collect::<HashSet<char>>()
-            .into_iter()
-            .collect::<Vec<char>>();
+            .filter(|antenna| antenna.frequency == frequency)
+            .collect::<Vec<_>>();
 
+        for i in 0..group.len() {
+            for j in i + 1..group.len() {
+                let a = group[i];
+                let b = group[j];
+                // compare with other antannae with same frequency.
+                // Add an antinode (one for each antennae) (as long as it lands within the bounds of the map)
+                // On the opposite side of the other, same distance away as the two antennae are from each other.
 
-        let mut antinode_locations: HashSet<Pos> = HashSet::new();
+                // This is slope of a traveling to b
+                let slope = slope(a, b);
 
-        for frequency in frequencies {
-            let group = antennae
-                .iter()
-                .filter(|antenna| antenna.frequency == frequency)
-                .collect::<Vec<_>>();
+                // check if antinode positions land within the bounds
 
-            for i in 0..group.len() {
-                for j in i + 1..group.len() {
-                    let a = group[i];
-                    let b = group[j];
-                    // compare with other antannae with same frequency.
-                    // Add an antinode (one for each antennae) (as long as it lands within the bounds of the map)
-                    // On the opposite side of the other, same distance away as the two antennae are from each other.
+                // antinode on other size of b (produced by a)
+                // Need to flip the signs on the slope rise/run
+                let mut antinode_a_pos = Pos {
+                    r: b.pos.r - slope.rise,
+                    c: b.pos.c - slope.run,
+                };
 
-                    // This is slope of a traveling to b
-                    let slope = slope(a, b);
-
-                    // check if antinode positions land within the bounds
-
-                    // antinode on other size of b (produced by a)
-                    // Need to flip the signs on the slope rise/run
-                    let antinode_a_pos = Pos {
-                        r: b.pos.r - slope.rise,
-                        c: b.pos.c - slope.run,
-                    };
-                    if !is_oob(&antinode_a_pos, r_bounds, c_bounds) {
-                        antinode_locations.insert(antinode_a_pos);
+                while !is_oob(&antinode_a_pos, grid.r_bounds, grid.c_bounds) {
+                    antinode_locations.insert(antinode_a_pos.clone());
+                    antinode_a_pos.r -= slope.rise;
+                    antinode_a_pos.c -= slope.run;
+                    if !harmonics {
+                        break;
                     }
+                }
 
-                    // antinode on other size of a (produced by b)
-                    let antinode_b_pos = Pos {
-                        r: a.pos.r + slope.rise,
-                        c: a.pos.c + slope.run,
-                    };
-                    if !is_oob(&antinode_b_pos, r_bounds, c_bounds) {
-                        antinode_locations.insert(antinode_b_pos);
+                // antinode on other size of a (produced by b)
+                let mut antinode_b_pos = Pos {
+                    r: a.pos.r + slope.rise,
+                    c: a.pos.c + slope.run,
+                };
+                while !is_oob(&antinode_b_pos, grid.r_bounds, grid.c_bounds) {
+                    antinode_locations.insert(antinode_b_pos.clone());
+                    antinode_b_pos.r += slope.rise;
+                    antinode_b_pos.c += slope.run;
+                    if !harmonics {
+                        break;
                     }
+                }
+
+                if harmonics {
+                    // add antennae a and b as they should also be considered antinodes
+                    antinode_locations.insert(a.pos.clone());
+                    antinode_locations.insert(b.pos.clone());
                 }
             }
         }
+    }
+    antinode_locations
+}
 
+impl Solution for Day08 {
+    fn part1(&self, input: &str) -> String {
+        let grid = parse(input);
+        let antinode_locations = create_antinodes(&grid, false);
         let count = antinode_locations.len();
         format!("{count}")
     }
 
     fn part2(&self, input: &str) -> String {
-        todo!()
+        let grid = parse(input);
+        let antinode_locations = create_antinodes(&grid, true);
+        let count = antinode_locations.len();
+        format!("{count}")
     }
 }
 
@@ -168,6 +202,14 @@ mod test {
     fn test_part1_example() {
         let expected = String::from("14");
         let actual = Day08.part1(INPUT);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let expected = String::from("34");
+        let actual = Day08.part2(INPUT);
 
         assert_eq!(actual, expected);
     }
