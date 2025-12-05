091a4c552b503858cf1288ef168f1e26efe75911
Max Jones <jonesmtuesday@gmail.com>
2025-12-04 23:53:40 -0500

    [go]: solve 2025 day 1 part 2

--- 2025/go/solution/day01.go
+++ 2025/go/solution/day01.go
@@ -12,8 +12,10 @@ func (d Day01) Part1(input string) string {
 	// L -> ((100 - x) + cur) mod 100
 	// R -> (x + cur) mod 100
 
-	count := 0
-	pos := 50
+	var (
+		count = 0
+		pos   = 50
+	)
 
 	lines := strings.SplitSeq(input, "\n")
 	for line := range lines {
@@ -38,5 +40,44 @@ func (d Day01) Part1(input string) string {
 }
 
 func (d Day01) Part2(input string) string {
-	panic("TODO: implement")
+	// Count every time 0 is passed
+	var (
+		count = 0
+		pos   = 50
+	)
+
+	lines := strings.SplitSeq(input, "\n")
+	for line := range lines {
+		dir := line[0]
+		num, _ := strconv.Atoi(line[1:])
+		switch dir {
+		case 'L':
+			// Don't go negative
+			if pos == 0 {
+				pos = 100
+			}
+			for _ = range num {
+				pos -= 1
+				if pos == 0 {
+					count += 1
+					pos = 100
+				}
+			}
+		case 'R':
+			// Don't go above 100
+			if pos == 100 {
+				pos = 0
+			}
+			for _ = range num {
+				pos += 1
+				if pos == 100 {
+					count += 1
+					pos = 0
+				}
+			}
+		default:
+			log.Fatalf("unknown first character: %c\n", dir)
+		}
+	}
+	return strconv.Itoa(count)
 }
--- 2025/go/solution/day01_test.go
+++ 2025/go/solution/day01_test.go
@@ -28,3 +28,30 @@ L82`
 		}
 	}
 }
+
+func TestDay01_Part2(t *testing.T) {
+	input := `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+
+	tts := []TestCase{
+		{
+			input:    input,
+			expected: "6",
+		},
+	}
+
+	for _, tt := range tts {
+		actual := Day01{}.Part2(tt.input)
+		if actual != tt.expected {
+			t.Errorf("Expected: %s; Got: %s", tt.expected, actual)
+		}
+	}
+}
