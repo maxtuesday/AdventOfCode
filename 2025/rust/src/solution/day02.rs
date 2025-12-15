d15bff68e120ec0133057d5bc67b63841bcd0091
Max Jones <jonesmtuesday@gmail.com>
2025-12-15 18:13:57 -0500

    [rust]: solve 2025 day 2 part 2

--- 2025/rust/src/solution/day02.rs
+++ 2025/rust/src/solution/day02.rs
@@ -16,6 +16,19 @@ fn parse(input: &str) -> Vec<(u64, u64)> {
         .collect::<_>()
 }
 
+fn count_digits(mut num: u64) -> u32 {
+    let mut count = 0;
+    while num > 0 {
+        num /= 10;
+        count += 1;
+    }
+    count
+}
+
+fn get_factors(num: u32) -> Vec<u32> {
+    (1..num).filter(|n| num % n == 0).collect()
+}
+
 impl Solution for Day02 {
     fn part1(&self, input: &str) -> String {
         let ranges = parse(input);
@@ -41,7 +54,35 @@ impl Solution for Day02 {
     }
 
     fn part2(&self, input: &str) -> String {
-        todo!()
+        let ranges = parse(input);
+
+        let total_ids = ranges
+            .into_iter()
+            .map(|(start, end)| {
+                let mut ids: Vec<u64> = Vec::new();
+                for num in start..=end {
+                    // break `num` up into substrings of the size of the factors of the number.
+                    let digits = count_digits(num);
+                    let factors = get_factors(digits);
+
+                    let num_str = num.to_string();
+                    for factor in factors {
+                        let substr = num_str.chars().take(factor as usize).collect::<String>();
+                        let times = digits / factor;
+                        let target = substr.repeat(times as usize);
+
+                        if target == num_str {
+                            ids.push(num);
+                            break;
+                        }
+                    }
+                }
+                ids
+            })
+            .flatten()
+            .sum::<u64>();
+
+        format!("{total_ids}")
     }
 }
 
@@ -55,6 +96,13 @@ mod tests {
     fn test_part1() {
         let expected = "1227775554";
         let actual = Day02.part1(INPUT);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_part2() {
+        let expected = "4174379265";
+        let actual = Day02.part2(INPUT);
         assert_eq!(expected, actual);
     }
 }
